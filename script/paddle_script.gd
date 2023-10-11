4d7b98b5ef02db3a168f97636ff858d20d33af53
jalvaradoWD <josealvarado1337@gmail.com>
2023-10-10 22:42:36 -0500

    Added more stuff.

--- script/paddle_script.gd
+++ script/paddle_script.gd
@@ -10,13 +10,13 @@ func _ready():
 
 func _input(_event):
 	if is_flipped:
-		if Input.is_action_just_pressed("paddle_right"):
+		if Input.is_action_just_pressed("flipper_right"):
 			print(scale)
-			PadAnimPlayer.play("paddle_right_animation")
-		elif Input.is_action_just_released("paddle_right"):
+			PadAnimPlayer.play("flipper_animation_right")
+		elif Input.is_action_just_released("flipper_right"):
 			PadAnimPlayer.play_backwards()
 	else:
-		if Input.is_action_just_pressed("paddle_left"):
-			PadAnimPlayer.play("paddle_left_animation")
-		elif Input.is_action_just_released("paddle_left"):
+		if Input.is_action_just_pressed("flipper_left"):
+			PadAnimPlayer.play("flipper_animation_left")
+		elif Input.is_action_just_released("flipper_left"):
 			PadAnimPlayer.play_backwards()
